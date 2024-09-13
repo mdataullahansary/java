3c24300e05f7f312899b69e645d2efee8766346e
mdataullahansary <mdataullahansari2003@gmail.com>
2024-09-13 15:42:33 +0530

    Type Casting

--- basic.java
+++ basic.java
@@ -1,17 +1,17 @@
 public class basic {
   public static void main(String[] args) {
-    String name = "Ata";
-    System.out.println(name);
-    // int myNum = 15;
-    // myNum = 20; overwrite the value 
-    final int myNum = 15 ;
-    // myNum = 25;  final variable can not asign
+    // String name = "Ata";
+    // System.out.println(name);
+    // // int myNum = 15;
+    // // myNum = 20; overwrite the value 
+    // final int myNum = 15 ;
+    // // myNum = 25;  final variable can not asign
     
-    //  float myfloatNum = 12.25;
-     char myLetter = 'D';
-     boolean myBool = true;
-     String myText = "Hello";
+    // //  float myfloatNum = 12.25;
+    //  char myLetter = 'D';
+    //  boolean myBool = true;
+    //  String myText = "Hello";
 
-    System.out.println(myNum);
+    // System.out.println(myNum);
   }
 }
---
+++ typeCasting.java
@@ -0,0 +1,43 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);      // Outputs 9
+    System.out.println(myDouble);   // Outputs 9.0
+
+    int maxScore =500;
+    int userScore = 427;
+    float percentage = (float) userScore / maxScore*100.0f;
+    System.out.println("User percentage: " + percentage);
+
+    int x = 28;
+    int y = 5;
+    System.out.println(x%y);   //give rimender
+
+    String name = "Md Ataullah Ansary";
+    System.out.println(name.length());
+    System.out.println(name.toUpperCase());
+    System.out.println(name.toLowerCase());
+    
+    String txt = "Believe you can and you’re halfway there";
+     System.out.println(txt.indexOf("and"));
+
+
+
+     String p = "10";
+     String q = "20";
+     String r = p + q;  // r will be 1020 (a String)   
+
+
+                        //    \n 	=  New Line	
+                        //    \r	=  Carriage Return	
+                        //    \t	=  Tab	
+                        //    \b	=  Backspace	
+                        //    \f	=  Form Feed
+
+
+
+
+    }
+}
