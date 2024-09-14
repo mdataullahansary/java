7b0dc4a1237636ff5eacdab74b247da21405a66f
mdataullahansary <mdataullahansari2003@gmail.com>
2024-09-14 11:34:44 +0530

    m

--- Method/parameter.java
+++ Method/parameter.java
@@ -1,14 +1,27 @@
 package Method;
 
 public class parameter {
-    static void myMethod (String lastname) {
-        System.out.println(lastname + "  Ansary");
-    }
+    // static void myMethod (String lastname) {
+    //     System.out.println(lastname + "  Ansary");
+    // }
     
-    public static void main(String[] args) {
-        myMethod("Ataullah");
-        myMethod("Asad");
-        myMethod("Mizu");
+    // public static void main(String[] args) {
+    //     myMethod("Ataullah");
+    //     myMethod("Asad");
+    //     myMethod("Mizu");
 
+    // }
+
+    static void checkedAge ( int age) {
+        if (age < 18) {
+            System.out.println("Access denied - You are not old enough!");
+        }
+        else  {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkedAge(20);
     }
 }
